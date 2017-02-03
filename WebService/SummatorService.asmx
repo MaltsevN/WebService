8ca2a935a2a09c22e9c4dc33915b7f6d90984915
Nikita Maltsev <nikitamalcev1@gmail.com>
2017-02-03 16:21:23 +0200

    Created

---
+++ WebService/SummatorService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="SummatorService.asmx.cs" Class="WebService.SummatorService" %>
---
+++ WebService/WebClient/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebClient.SummatorService;
+
+namespace WebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var client = new SummatorServiceSoapClient();
+            Label1.Text = client.GetSum(int.Parse(TextBox1.Text), int.Parse(TextBox2.Text)).ToString();
+        }
+    }
+}
